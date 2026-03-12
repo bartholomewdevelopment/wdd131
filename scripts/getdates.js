9c8870a809b514e8ec355e1e905a4f916d8c244e
bartholomewdevelopment <joseph@bartholomewdevelopment.com>
2026-03-12 08:26:49 -0400

    Complete W01 home page assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+document.getElementById("lastModified").textContent =
+  `Last Modified: ${document.lastModified}`;
